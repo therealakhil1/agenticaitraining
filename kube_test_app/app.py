a99a002f97c39074e3b29c6eab6b2450085c1588
therealakhil1 <akhilspalli@gmail.com>
2025-05-10 11:09:19 -0400

    Adding health check to fast api

--- kube_test_app/app.py
+++ kube_test_app/app.py
@@ -6,6 +6,14 @@ app = FastAPI(title="Interview Agent API")
 async def root():
     return {'hello':'world from akhil'}
 
+@app.get("/healthz", tags=["health"])
+async def healthz():
+    """
+    Simple liveness/readiness check.
+    Returns HTTP 200 if the app is up.
+    """
+    return {"status": "healthy"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
